cc05963681503161cbd129cd699b797f7beeb457
carsten.lex <carsten.lex@gmail.com>
2017-12-11 15:01:27 +0100

    Matches speichern und Team-Referenz: entweder neues Team erzeugen, oder bestehendes Team referenzieren

--- src/main/java/com/toeggeli/toeggeli/match/MatchController.java
+++ src/main/java/com/toeggeli/toeggeli/match/MatchController.java
@@ -1,5 +1,7 @@
 package com.toeggeli.toeggeli.match;
 
+import com.toeggeli.toeggeli.team.Team;
+import com.toeggeli.toeggeli.team.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -12,28 +14,47 @@ public class MatchController {
     @Autowired
     MatchRepository matchRepository;
 
+    @Autowired
+    TeamRepository teamRepository;
+
     @GetMapping
-    public Flux<Match> listMatches (){
+    public Flux<Match> listMatches() {
         return matchRepository.findAll();
     }
 
     @GetMapping("/{id}")
-    public Mono<Match> findMatch (@PathVariable("id") String id){
+    public Mono<Match> findMatch(@PathVariable("id") String id) {
         return matchRepository.findById(id);
     }
 
     @DeleteMapping("/{id}")
-    public Mono<Void> deleteMatch (@PathVariable("id") String id){
+    public Mono<Void> deleteMatch(@PathVariable("id") String id) {
         //TODO orphan - remove Teams? oder wegen History drin lassen
         return matchRepository.deleteById(id);
     }
 
 
-
     @PostMapping
-    public Mono<Match> saveMatch(@RequestBody Match match){
+    public Mono<Match> saveMatch(@RequestBody Match match) {
+        //FIXME das ist die blocking-variante
+        Team team1 = findOrCreateTeamBLOCKED(match.getTeam1());
+        Team team2 = findOrCreateTeamBLOCKED(match.getTeam2());
+        match.setTeam2(team2);
+        match.setTeam1(team1);
         Mono<Match> save = matchRepository.save(match);
-        //TODO upsert die beiden Teams
         return save;
+
+    }
+
+    private Team findOrCreateTeamBLOCKED(Team team) {
+        if (team.getId() != null) {
+            return teamRepository.findById(team.getId()).block();
+        }
+        Team byPlayers = teamRepository.findByPlayers(team.getPlayer1().getName(), team.getPlayer2().getName()).block();
+        if (byPlayers == null) {
+            return teamRepository.save(team).block();
+        } else {
+            return byPlayers;
+        }
     }
 }
--- src/main/java/com/toeggeli/toeggeli/team/TeamRepository.java
+++ src/main/java/com/toeggeli/toeggeli/team/TeamRepository.java
@@ -7,6 +7,6 @@ import reactor.core.publisher.Mono;
 
 public interface TeamRepository extends ReactiveMongoRepository<Team, String>{
 
-    @Query("{ '$or' : [{'player1.name': ?0} ,{'player2.name': ?0 } ] ,  '$or' : [{'player1.name': ?1} ,{'player2.name': ?1 } ] }")
+    @Query("{ $and:[{ '$or' : [{'player1.name': ?0} ,{'player2.name': ?0 } ] },  {'$or' : [{'player1.name': ?1} ,{'player2.name': ?1 } ] } ] }")
     Mono<Team> findByPlayers(String namePlayer1, String namePlayer2);
 }
