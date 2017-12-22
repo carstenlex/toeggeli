f5539afe516d6d9ca8c8e85a8e00f55794bcb33b
Phong6698 <Phong@hotmail.ch>
2017-12-22 15:54:00 +0100

    StatistikController -> Crossorigin

--- src/main/java/com/toeggeli/toeggeli/statistik/StatistikController.java
+++ src/main/java/com/toeggeli/toeggeli/statistik/StatistikController.java
@@ -1,12 +1,14 @@
 package com.toeggeli.toeggeli.statistik;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/statistik")
 public class StatistikController {
